7aa929421c101ed4c07a91afc98960d0b5b14ffc
nelmish <standarduser@fitnessunchained.de>
2021-08-10 19:26:17 +0200

    166. Join Page & 167. Socket.io Rooms

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
-const {generateMessage, generateLocationMessage} = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,8 +18,16 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+
+
+    })
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -28,13 +36,13 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!')
         }
 
-        io.emit('message', generateMessage(message))
+        io.to('generalroom').emit('message', generateMessage(message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-            io.emit('locationMessage', generateLocationMessage(`https://www.google.de/maps?q=${coords.latitude},${coords.longitude}`))
-            callback()
+        io.emit('locationMessage', generateLocationMessage(`https://www.google.de/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
     })
 
     socket.on('disconnect', () => {
